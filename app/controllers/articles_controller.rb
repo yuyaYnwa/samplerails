e132f49e1b71bd7ca9b94f0e017c1d88aa7c8e9a
YuyaSakumae <y-sakumae@atgs.jp>
2015-07-21 17:11:56 +0900

    記事の修正と、新規記事の作成・登録の機能を実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,18 +9,35 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def create
+    @article = Article.new(params[:article])
+    if @article.save
+      redirect_to @article,notice:"ニュース記事を登録しました"
+    else
+      render "new"
+    end
   end
 
   def update
+    @article = Article.find(params[:id])
+    @article.assign_attributes(params[:article])
+    if @article.save
+      redirect_to @article,notice: "ニュース記事を更新しました"
+    else
+      render "edit"
+    end
   end
 
   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to :articles
   end
-
 end
--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -7,10 +7,59 @@ class ArticlesControllerTest < ActionController::TestCase
     assert_response :success
     assert_equal 5, assigns(:articles).count
   end
+
   test "show" do
     article = FactoryGirl.create(:article,expire_at:nil)
     #showアクションを呼び出す
     get :show,id:article
     assert_response :success
   end
+
+  test "new" do
+    get :new
+    assert_response :success
+  end
+
+  test "edit" do
+    article = FactoryGirl.create(:article)
+    get :edit, id: article
+    assert_response :success
+  end
+
+  test "create" do
+    post :create,article:FactoryGirl.attributes_for(:article)
+    article = Article.order(:id).last
+    assert_redirected_to article
+  end
+
+  test "update" do
+    article = FactoryGirl.create(:article)
+    patch :update, id: article, article: FactoryGirl.attributes_for(:article)
+    assert_redirected_to article
+  end
+
+  test "fail to create" do
+    attrs = FactoryGirl.attributes_for(:article,title: "")
+    post :create,article: attrs
+    assert_response :success
+    assert_template "new"
+  end
+
+  test "fail to update" do
+    attrs = FactoryGirl.attributes_for(:article,body: "")
+    article = FactoryGirl.create(:article)
+    put :update, id: article, article: attrs
+    assert_response :success
+    assert_template "edit"
+  end
+
+  test "destroy" do
+    article = FactoryGirl.create(:article)
+    delete :destroy,id: article
+    assert_redirected_to :articles
+    #本当に記事が削除されたかどうか？
+    assert_raises(ActiveRecord::RecordNotFound){
+      Article.find(article.id)}
+  end
+
 end
